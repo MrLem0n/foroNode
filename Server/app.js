852508035633009f3780cddd8966518c63fd4f03
Lemon <lisandro.m.yo@gmail.com>
2023-09-28 22:02:02 -0300

    problemas con edit

--- Server/app.js
+++ Server/app.js
@@ -6,11 +6,13 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
+
 app.use(cors());
 
 const port = 3000;
-
 app.use(`/`, forumRouter);
+
+
 
 app.listen(port, () => {
   console.log(`Servidores express en http://localhost:${port}`);
--- Web/scipt.js
+++ Web/scipt.js
@@ -2,9 +2,11 @@ const contenedor = document.getElementById("row-container");
 const btnCreate = document.getElementById("btn-mod");
 const myModal = new bootstrap.Modal(document.getElementById("myModal"));
 const btnSave = document.getElementById("btn-save");
+const forma = document.getElementById("formulario");
 
 let html = "";
 let option = "";
+let formID="";
 
 const inputTitle = document.getElementById("InpuTitle");
 const inputDesc = document.getElementById("inputDesc");
@@ -21,10 +23,11 @@ btnCreate.addEventListener("click", () => {
 
 document.addEventListener("click", (event) => {
   if (event.target.matches("#btn-delete")) {
+    
     const article = event.target.closest(".col-4");
     const idArticle = article.dataset.id;
 
-    fetch(`http://localhost/api/posts/${idArticle}`, { method: `DELETE` })
+    fetch(`http://localhost:3000/api/posts/${idArticle}`, { method: "DELETE"})
       .then((res) => {
         if (res.ok) {
           article.remove();
@@ -36,16 +39,103 @@ document.addEventListener("click", (event) => {
   }
 });
 
+// document.addEventListener("click", (event) => {
+//   if (event.target.matches("#btn-edit")) {
+//     const article = event.target.closest(".col-4");
+
+//     const idArticle= article.dataset.id;
+//     const urlImage = article.dataset.children[0].src;
+//     const TitlEdit=  article.children[0].children[1].children[0].textContent;
+//     const descriEdit=article.children[0].children[1].children[1].textContent;
+    
+//     idForm = idArticle;
+//     inputTitle.value = titleEdit;
+//     inputDescription.value = descriptionEdit;
+//     inputPoster.value = urlPosterEdit;
+//     option = "edit";
+//     btnSave.textContent = "Edit";
+//     myModal.show();
+//   }
+// });
+// crear post
+ forma.addEventListener("submit", (event) => {
+   event.preventDefault();
+   
+   if (option === "new") {
+     const newPost = {
+       tittle: inputTitle.value,
+       description: inputDesc.value,
+       image: inputImage.value,
+     };
+
+     fetch('http://localhost:3000/api/posts', {
+       method: 'POST',
+       headers: {
+        'Content-Type': 'application/json'
+       },
+       body: JSON.stringify(newPost)
+     }).then(res => {
+       console.log(res)
+         if (res.ok) {
+           alert("Post created successfully");
+           myModal.hide();
+           location.reload();
+         }
+       })
+       .catch((err) => {
+         console.error(err);
+       });
+   }
+
+   if (option === "edit") {
+     const newPost = {
+       tittle: inputTitle.value,
+       description: inputDesc.value,
+       image: inputImage.value,
+     };
+
+     fetch(`http://localhost:3000/api/tasks/${formID}`, {
+       method: 'PUT',
+       headers: {
+         'Content-Type': 'application/json'
+       },
+       body: JSON.stringify(newPost)
+     }).then(res => {
+      console.log(res)
+       if(res.ok){
+       alert('Post edited successfully')
+         myModal.hide();
+        location.reload();
+       }
+     })
+   }
+ });
+// Editar 
 document.addEventListener("click", (event) => {
   if (event.target.matches("#btn-edit")) {
     const article = event.target.closest(".col-4");
 
-    const idArticle= article.dataset.id;
-    const urlImage = article.dataset.children[0].children.src;
-    console.log(urlImage);
-   
+    const idArticle = article.dataset.id;
+    const urlImageEdit = article.children[0].children[0].src;
+    const titleEdit = article.children[0].children[1].children[0].textContent;
+    const descriptionEdit = article.children[0].children[1].children[1].textContent;
+
+    formID = idArticle;
+    inputTitle.value = titleEdit;
+    inputDesc.value = descriptionEdit;
+    inputImage.value = urlImageEdit;
+    option = "edit";
+    btnSave.textContent = "edit";
+    myModal.show();
   }
 });
+
+
+
+
+
+
+//render
 fetch("http://localhost:3000/api/posts")
   .then((response) => response.json())
   .then((data) => {
