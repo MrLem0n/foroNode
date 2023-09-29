dd25f809f1c4d5ba0cbe2393c1db9726e1546a12
Lemon <lisandro.m.yo@gmail.com>
2023-09-29 10:39:25 -0300

    update

--- Web/scipt.js
+++ Web/scipt.js
@@ -6,7 +6,7 @@ const forma = document.getElementById("formulario");
 
 let html = "";
 let option = "";
-let formID="";
+let formID = "";
 
 const inputTitle = document.getElementById("InpuTitle");
 const inputDesc = document.getElementById("inputDesc");
@@ -23,11 +23,10 @@ btnCreate.addEventListener("click", () => {
 
 document.addEventListener("click", (event) => {
   if (event.target.matches("#btn-delete")) {
-    
     const article = event.target.closest(".col-4");
     const idArticle = article.dataset.id;
 
-    fetch(`http://localhost:3000/api/posts/${idArticle}`, { method: "DELETE"})
+    fetch(`http://localhost:3000/api/posts/${idArticle}`, { method: "DELETE" })
       .then((res) => {
         if (res.ok) {
           article.remove();
@@ -39,78 +38,60 @@ document.addEventListener("click", (event) => {
   }
 });
 
-// document.addEventListener("click", (event) => {
-//   if (event.target.matches("#btn-edit")) {
-//     const article = event.target.closest(".col-4");
-
-//     const idArticle= article.dataset.id;
-//     const urlImage = article.dataset.children[0].src;
-//     const TitlEdit=  article.children[0].children[1].children[0].textContent;
-//     const descriEdit=article.children[0].children[1].children[1].textContent;
-    
-//     idForm = idArticle;
-//     inputTitle.value = titleEdit;
-//     inputDescription.value = descriptionEdit;
-//     inputPoster.value = urlPosterEdit;
-//     option = "edit";
-//     btnSave.textContent = "Edit";
-//     myModal.show();
-//   }
-// });
 // crear post
- forma.addEventListener("submit", (event) => {
-   event.preventDefault();
-   
-   if (option === "new") {
-     const newPost = {
-       tittle: inputTitle.value,
-       description: inputDesc.value,
-       image: inputImage.value,
-     };
+forma.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-     fetch('http://localhost:3000/api/posts', {
-       method: 'POST',
-       headers: {
-        'Content-Type': 'application/json'
-       },
-       body: JSON.stringify(newPost)
-     }).then(res => {
-       console.log(res)
-         if (res.ok) {
-           alert("Post created successfully");
-           myModal.hide();
-           location.reload();
-         }
-       })
-       .catch((err) => {
-         console.error(err);
-       });
-   }
+  if (option === "new") {
+    const newPost = {
+      tittle: inputTitle.value,
+      description: inputDesc.value,
+      image: inputImage.value,
+    };
 
-   if (option === "edit") {
-     const newPost = {
-       tittle: inputTitle.value,
-       description: inputDesc.value,
-       image: inputImage.value,
-     };
+    fetch("http://localhost:3000/api/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPost),
+    })
+      .then((res) => {
+        if (res.ok) {
+          alert("Post created successfully");
+          myModal.hide();
+          location.reload();
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 
-     fetch(`http://localhost:3000/api/tasks/${formID}`, {
-       method: 'PUT',
-       headers: {
-         'Content-Type': 'application/json'
-       },
-       body: JSON.stringify(newPost)
-     }).then(res => {
-      console.log(res)
-       if(res.ok){
-       alert('Post edited successfully')
-         myModal.hide();
+  if (option === "edit") {
+    const newPost = {
+      tittle: inputTitle.value,
+      description: inputDesc.value,
+      image: inputImage.value,
+    };
+
+    fetch(`http://localhost:3000/api/posts/${formID}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPost),
+    }).then((res) => {
+      console.log(res);
+      if (res.ok) {
+        alert("Post edited successfully");
+        myModal.hide();
         location.reload();
-       }
-     })
-   }
- });
-// Editar 
+      }
+    });
+  }
+});
+// Editar
 document.addEventListener("click", (event) => {
   if (event.target.matches("#btn-edit")) {
     const article = event.target.closest(".col-4");
@@ -118,7 +99,8 @@ document.addEventListener("click", (event) => {
     const idArticle = article.dataset.id;
     const urlImageEdit = article.children[0].children[0].src;
     const titleEdit = article.children[0].children[1].children[0].textContent;
-    const descriptionEdit = article.children[0].children[1].children[1].textContent;
+    const descriptionEdit =
+      article.children[0].children[1].children[1].textContent;
 
     formID = idArticle;
     inputTitle.value = titleEdit;
@@ -127,13 +109,9 @@ document.addEventListener("click", (event) => {
     option = "edit";
     btnSave.textContent = "edit";
     myModal.show();
+    console.log(idArticle, urlImageEdit, descriptionEdit, titleEdit);
   }
 });
-
-
-
-
-
 
 //render
 fetch("http://localhost:3000/api/posts")
--- readme.md
+++ readme.md
@@ -1,3 +1,31 @@
-##Comision Comisión 21646 A- Lisandro Martin
+<h1 align="center">Comision Comisión 21646 A- Lisandro Martin</h1>
 
-#Proyecto node js
+<h2>Proyecto foro multitematico</h2>
+
+<p><b>descripcion:</b></p>
+
+<p>Foro multitematico desarrolado en node.js</p>
+
+### Lenguajes utilizados
+
+<ul>
+<li>HTML5</li>
+<li>CSS</li>
+<li>Javascript</li>
+<li>Node.JS</li>
+</ul>
+
+### Instrucciones de uso
+
+<ul>
+<li>En caso de no tener xampp, descargarlo e instalarlo para facilitar el uso del programa</li>
+<li>presione los botones "start" en <b>Apache</b> y <b>MySQL</b></li>
+<li>Posicionarse en la carpeta "Server"</li>
+<li>Abrir la misma en el terminal integrado</li>
+<li>ejecutar el comando: $npm run dev</li>
+
+</ul>
+
+### Elementos faltantes
+
+<p>Faltan: Variables de entorno, ejs y path<p>
